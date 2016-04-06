b04df011690824ddc286a2cc137d781e00f0f8d6
Jeremy Patterson <jrmypttrsn@gmail.com>
2016-04-05 23:12:29 -0500

    Renamed temp converter, results, and home page erb

--- main.rb
+++ main.rb
@@ -4,7 +4,7 @@ require "sinatra"
 # Your routes go here:
 
 get '/' do
-	erb :home_page
+	erb :root_path
 end
 
 get '/temp-converter' do
@@ -12,5 +12,30 @@ get '/temp-converter' do
 end
 
 get '/temp-converter-results' do
+	@temperature = params['temperature'].to_i
+	@units = params['units']
 	erb :temp_converter_results
+end
+
+
+
+
+
+############################################
+def temperature_conversion(temperature, units)
+	if units == "c"
+		converted_temperature = (temperature * 9/5) + 32
+	else
+		converted_temperature = (temperature - 32) * 5/9
+	end
+	converted_temperature
+end
+
+def converted_unit(units)
+	if units == "f"
+		converted_unit = "c"
+	else
+		converted_unit = "f"
+	end
+	converted_unit
 end
